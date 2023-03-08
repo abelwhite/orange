5c1d62279272d1920f76233f3b7b408d7b86908e
Abel Blanco <2018117977@ub.edu.bz>
2023-03-08 15:13:47 -0600

    added school struct

--- internal/data/schools.go
+++ internal/data/schools.go
@@ -1,3 +1,22 @@
-//filename: internal/data/schools.go
+// filename: internal/data/schools.go
 package data
 
+import (
+	"time"
+
+	"honnef.co/go/tools/lintcmd/version"
+)
+
+type School struct{
+	ID int64
+	Name string
+	Level string
+	Contact string
+	Phone string
+	Email string
+	Website string
+	Address string
+	Mode []string
+	CreatedAt time.Time
+	version int32
+}
