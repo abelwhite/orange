09bf71774b5cf9bb59876c384bfa8d6e1d23b9cb
Abel Blanco <2018117977@ub.edu.bz>
2023-03-20 15:21:24 -0600

    decoded raw data

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -10,7 +11,28 @@ import (
 )
 
 func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Created a school...")
+	//create a struct to hold a school that will be provided to us
+	//via the request
+	var input struct {
+		Name    string   `json:"name"`
+		Level   string   `json:"level"`
+		Contact string   `json:"contact"`
+		Phone   string   `json:"phone"`
+		Email   string   `json:"email"`
+		Website string   `json:"website,omitempty"`
+		Address string   `json:"address"`
+		Mode    []string `json:"mode"`
+	}
+	//Initialize a new json.Decoder instance
+	err := json.NewDecoder(r.Body).Decode(&input) //we take r.Body and decode it into input
+	//take it from json and decode it so it can be sent to db.
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+	//Print the request
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 func (app *application) showSchoolHandler(w http.ResponseWriter, r *http.Request) {
