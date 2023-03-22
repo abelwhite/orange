abba185c8c30411245ecd5ce56430a22fe14e3b7
Abel Blanco <2018117977@ub.edu.bz>
2023-03-22 15:17:50 -0600

    added json request error checking

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -40,3 +40,5 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+//comment
